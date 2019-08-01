af7611a256c6ab78cfe045ad4a5213921f819a4a
Tracy Reuther <tracyreuther@gmail.com>
2019-08-01 14:55:07 +0000

    coffee.rb complete

--- test_project/lib/coffee_sale/cli.rb
+++ test_project/lib/coffee_sale/cli.rb
@@ -2,9 +2,8 @@ class CoffeeSale::CLI
   
   def call
     puts "Welcome to CoffeeIcon: Specials - Enjoy limited time deals on select brands".colorize(:light_blue)
-    # get_coffees
-    # list_coffees
-    # get_user_selection
+    # create_from_collection(cofees_array)
+    # add_coffee_attributes(attributes_hash)
     # show_coffee_info(name)
   end
   
--- test_project/lib/coffee_sale/coffee.rb
+++ test_project/lib/coffee_sale/coffee.rb
@@ -1,5 +1,29 @@
 class CoffeeSale::Coffee
   
+  attr_accessor :name, :orig_price, :sale_price
   
+  @@all = []
+  
+  def initialize(student_hash)
+    student_hash.each {|k, v| self.send(("#{k}="), v)}
+    @@all << self
+  end
+  
+  def self.create_from_collection(coffees_array)
+    coffees_array.each do |coffees_info|
+      self.new(coffees_info)
+    end
+  end
+  
+  def add_coffee_attributes(attributes_hash)
+    attributes_hash.each do |k,v|
+      self.send(("#{k}="), v)
+    end
+    self
+  end
+  
+  def self.all
+    @@all
+  end
   
 end
